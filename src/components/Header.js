10da67ff5e7177d28b142827095e5e58be351c76
cephscope <sworm.null@gmail.com>
2018-08-15 18:09:52 -0400

    fixed typo in header title

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header className="App-header">
-    <h1 className="App-title">Driodify</h1>
+    <h1 className="App-title">Droidify</h1>
     <h1 className="App-title">Build-a-Droid</h1>
     <div className="nav">
       <p className="nav-item">Shop:</p>
