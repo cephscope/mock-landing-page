ad899c7a78f17e14dc9c8ef0042e59127e80ed23
cephscope <sworm.null@gmail.com>
2018-08-16 15:00:23 -0400

    optimized for mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Footer = () => (
   <div className="footer">
     Mock landing page by <a href="https://roycathey.org/" target="_blank" rel="noopener noreferrer">Roy Cathey</a>
+    <br />
+    <br />
+    <a href="https://github.com/cephscope/mock-landing-page">View project on GitHub</a>
   </div>
 );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <header className="App-header">
     <h1 className="App-title">Droidify</h1>
-    <h1 className="App-title">Build-a-Droid</h1>
+    <h1 className="App-title" id="bad">Build-a-Droid</h1>
     <div className="nav">
       <p className="nav-item">Shop:</p>
       <a className="nav-item" href="#">Andriods</a>
