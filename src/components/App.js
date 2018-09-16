f955a0f52252a11c25c78fe619b245d632199743
cephscope <sworm.null@gmail.com>
2018-09-16 17:40:33 -0400

    fetching images from web server

--- src/components/App.js
+++ src/components/App.js
@@ -38,7 +38,7 @@ class App extends React.Component {
         <Header />
         <div className="video">
           <video id="video-long" src="https://res.cloudinary.com/dnqc6pmtp/video/upload/v1537132089/robott.mp4" autoPlay loop muted />
-          <img className="gif" id="giftop" src="./robott.gif" alt="droids"/>
+          <img className="gif" id="giftop" src="https://res.cloudinary.com/dnqc6pmtp/image/upload/v1537133610/landing-page/robott.gif" alt="droids"/>
         </div>
         <CopyContainer
           selectedCopy={this.state.copy}
@@ -48,14 +48,14 @@ class App extends React.Component {
         />
         <div className="video" id="boxed-video">
           <video id="boxed" src="https://res.cloudinary.com/dnqc6pmtp/video/upload/v1537132465/landing-page/roboto.mp4" autoPlay loop muted />
-          <img className="gif" src="./roboto.gif" alt="droids"/>
+          <img className="gif" src="https://res.cloudinary.com/dnqc6pmtp/image/upload/v1537133794/landing-page/roboto-gif.gif" alt="droids"/>
           <p>Model #451233548 meeting his creator</p>
           <a href="#">Start building your new droid</a>
         </div>
         <ImageContainer />
         <div className="video" id="end-video">
           <video id="boxed" src="https://res.cloudinary.com/dnqc6pmtp/video/upload/v1537132561/landing-page/sophia.mp4" autoPlay loop muted />
-          <img className="gif" src="./sophia.gif" alt="droids"/>
+          <img className="gif" src="https://res.cloudinary.com/dnqc6pmtp/image/upload/v1537133836/landing-page/sophiagif.gif" alt="droids"/>
           <p>Model P-Sophia</p>
           <a href="#">Shop presets</a>
         </div>
